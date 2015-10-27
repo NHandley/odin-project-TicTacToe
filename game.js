e2166ec9561315d00c66ee1be0648594500ba7a0
NHandley <nathan.handley1@gmail.com>
2015-10-27 14:51:08 +1100

    Logic update
    Added condition for squares already selected

--- game.js
+++ game.js
@@ -1,8 +1,7 @@
 (function() {
 
-	var currentPlayer = 'player1'
+	var currentPlayer = 'player1';
 
-	//Function to 
 	var switchPayer = function() {
 		switch(currentPlayer) {
 		case 'player1':
@@ -13,15 +12,30 @@
 			break;
 		}
 	};
+
 	var board = [$('<div>')];
 
+	//loop to create grid
 	for (var i = 0; i < 9; i++) {
 		$('.container').append('<div id="square-'+i+'" class="square">');
 	}
 
-	$('.square').on("click", function(e) {
-		$(this).addClass(currentPlayer);
-		switchPayer();
+	$('.square').on("click", function() {
+
+		//Conditions for pre selected squares
+		if($(this).hasClass("player1") || $(this).hasClass("player2") ){
+			console.log("No clicky");
+		}
+		else{
+
+			$(this).addClass(currentPlayer);
+			switchPayer();
+		}
+
+		//Condition for win or lose
+		for (var i = ..; i >= 0; i++) {
+			...
+		};
 	})
 
 })();
